11d40d8a8b0d1272645d86a45b5a51ccd97f28ac
AntonyOnScript <santhiagoafk@gmail.com>
2021-11-15 19:33:25 -0300

    limpou espaços em branco do código

--- index.c
+++ index.c
@@ -16,17 +16,14 @@ int op;
 
 void cadastro();
 
-typedef struct 
-{
+typedef struct {
     char login[30];
     char senha[30];
-} pessoa; pessoa p[1]; //Nome da estrutura, diminiu o valor de pessoa para p[1];
+} 
 
+pessoa; pessoa p[1]; //Nome da estrutura, diminiu o valor de pessoa para p[1];
 
-
-
-int main(){
-
+int main() {
     char login[30]; //Esse vetor só existe dentro da função Main() Por isso não existe conflito com a struct;
     char senha[30];
 
@@ -39,25 +36,21 @@ int main(){
     printf("\nsenha:");
     scanf("%s", senha);
 
-  
-
-
     /* strcmp - essa função é responsável por comparar strings   */
-    if((strcmp(login, p[0].login)==0) && (strcmp(senha, p[0].senha)==0)){
+    if((strcmp(login, p[0].login)==0) && (strcmp(senha, p[0].senha)==0)) {
         printf("Usuário logado.\n");
         cadastro();
-    }else{
+    } else {
         printf("Login e/ou senha incorretos.\n");
     }
 
     return 0;
 }
 
-
 void cadastro(){
     static int linha;
-    do{
 
+    do{
         printf("\nDigite o nome do paciente: ");
         scanf("%s", nome[linha]);
 
@@ -76,7 +69,5 @@ void cadastro(){
         printf("\nDigite 1 para continuarou outro valor para sair.");
         scanf("%d", &op);
         linha++;
-
-    }while(op==1);
-
+    } while(op==1);
 }
